50d49264c32bb8b9c5001231bb142ce6eb862a61
alios <abc.ua@yandex.ru>
2014-02-10 03:06:48 +0200

    up

--- examples/upload.js
+++ examples/upload.js
@@ -1,75 +1,17 @@
 var fotki = require('fotki');
 
-fotki.auth('username', 'token');
-
-(function() {
-    var album;
-
-    var clear = function() {
-        fotki.albums.getCollection()
-            .then(function(albumCollection) {
-                albumCollection.entries.forEach(function(value) {
-                    fotki.albums.delete(value);
-                });
-            });
-    };
-
-    var job = function() {
-        fotki.albums
-            .getByName('Неразобранное')
-            .then(function(data) {
-                album = data;
-
-                return fotki.albums.clear(data);
-            }, function() {
-                return fotki.albums
-                    .create({
-                        title: 'Неразобранное',
-                        summary: 'test description'
-                    })
-                    .then(function() {
-                        var returnDeferred = fotki.albums.getByName('Неразобранное');
-
-                        returnDeferred
-                            .then(function(data) {
-                                album = data;
-                                console.log(1, data);
-                            }, function() {
-                                console.log(3, arguments);
-                            });
-
-                        return returnDeferred;
-                    });
-            })
-            .then(function() {
-                return fotki.photos.upload(album, __dirname + '/1.png');
-            })
-            .then(function() {
-                return fotki.photos.get(album);
-            })
-            .then(function(photos) {
-                photos.entries.forEach(function(value) {
-                    fotki.photos.edit(value, {
-                        title: 'test',
-                        access: 'public'
-                    });
-                });
-            })
-            .fail(function() {
-                console.log(9, arguments);
-            });
-    };
-
-//    clear();
-//    job();
-
-    fotki.albums.getByName('inner')
-        .then(function(data) {
-            return fotki.photos.upload(data[0].links.photos, __dirname + '/1.png');
-        })
-        .then(function(data) {
-            return fotki.photos.edit(data, {
-                access: 'public'
-            });
+fotki
+    // set authorize data
+    .auth('login', 'token')
+    // get target album
+    .albums.getByName('albumName')
+    // upload image
+    .then(function(albums) {
+        return fotki.photos.upload(albums[0].links.photos, 'pathToFile');
+    })
+    // edit photo: change access level
+    .then(function(photo) {
+        return fotki.photos.edit(photo, {
+            access: 'public'
         });
-}());
+    });
